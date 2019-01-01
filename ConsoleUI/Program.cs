8930f6aa51f2a3c3d57328b383fdb811c8f87f2a
Egor Kostan <igorkostan@gmail.com>
2019-01-01 14:28:57 -0800

    Get user input and display it in console window + exit functionality

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleUI
@@ -14,7 +15,34 @@ namespace ConsoleUI
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.Read(); //Keep console window open
+            //App params
+            string greetings = "Hello dear user.\n";
+            string enterInput = "Please enter your input (or type 'exit' in order to close the app):\n";
+            string exit = "exit";
+            string exitApp = "The application will close now";
+            int sleepTime = 3000;
+
+            Console.Write(greetings); //Greetings message
+
+            while (true) {
+
+                Console.Write(enterInput); //Ask for user input
+
+                //Keep console window open
+                //Get user input and convert it into a string
+                string userInput = Console.ReadLine().ToString().Trim();
+
+                //Close app if user type "exit"
+                if (userInput.ToLower().Equals(exit)) {
+
+                    Console.Write(exitApp);
+                    Thread.Sleep(sleepTime);
+                    Environment.Exit(0);
+                }
+
+                //Display user input
+                Console.Write("User input: " + userInput + "\n");
+            }           
         }
     }
 }
